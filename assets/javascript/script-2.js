7815cf755bd183503575b9565b5ebf88863ba26b
Skelatorian <mylynhle21@gmail.com>
2020-08-30 21:36:53 -0400

    add js files

---
+++ assets/javascript/script-2.js
@@ -0,0 +1,23 @@
+//for the score page
+
+var scoreCard = JSON.parse(localStorage.getItem("user-score-card"));
+var olEl = document.querySelector("#display-score");
+var clearHighScoreEl = document.querySelector("#clear-highscores");
+
+function renderLastUserScoreCard() {
+  olEl = document.querySelector("#display-score");
+  liEl = document.createElement("li");
+  liEl.textContent = scoreCard["user-initial"] + " - " + scoreCard["user-score"];
+  olEl.appendChild(liEl);
+}
+
+clearHighScoreEl.addEventListener("click", function(event) {
+  event.preventDefault();
+  $("#display-score").empty();
+  localStorage.clear();
+});
+
+renderLastUserScoreCard();
+
+
+
